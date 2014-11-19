9778636d1f55613273a78fd4036ac3d5b510a487
Simon Edwards <simon@gx.nl>
2014-11-19 15:10:09 +0100

    Fixed OS detection deep in the node emulation for Rhino.

--- src/main/resources/modules/os.js
+++ src/main/resources/modules/os.js
@@ -2,6 +2,14 @@ exports.EOL = (function() {
     return java.lang.System.getProperty("line.separator");
 })();
 exports.platform = function() {
-  return "win64"; // FIXME
+  var osArch = java.lang.System.getProperty("os.arch");
+  var osName = java.lang.System.getProperty("os.name", "generic").toLowerCase(java.util.Locale.ENGLISH);
+  if (osName.indexOf("mac") >= 0 || osName.indexOf("darwin") >= 0) {
+    return "darwin";
+  } else if (osName.indexOf("win") >= 0) {
+    return osArch.indexOf("64") >= 0 ? "win64" : "win32";
+  } else {
+    return osName;
+  }
 };
 
